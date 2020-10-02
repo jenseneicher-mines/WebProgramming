9b2660e5d98bce307e473a5af57ba8d4330324e0
Jensen Eicher <jeicher@mymail.mines.edu>
2020-10-02 17:42:46 -0600

    hw6 update

--- hw6/quiz.js
+++ hw6/quiz.js
@@ -1,7 +1,3 @@
-function check() {
-    document.getElementById("Radio1").checked = true;
-    document.getElementById("Radio2").checked = false;
-}
 $(function() {
     var category;
     let score = 0;
@@ -9,12 +5,17 @@ $(function() {
     let attempted = "";
     let arr = [];
     let totalScore = 0;
+    $(document.getElementById("l1")).hide();
+    $(document.getElementById("l4")).hide();
 
     $("#home").click(function () {
         score = 0;
         i = 0;
-        document.getElementById("next").disabled = false;
+        $(document.getElementById("next").disabled = false);
         $(document.getElementById("l1")).text("Question: ");
+        $(document.getElementById("l1")).show();
+        $(document.getElementById("l4")).show();
+        $(document.getElementById("l5")).hide();
         window.category = {
             questions: ['A Pits Model 12 is an aerobatic biplane', 'An RV-6 has two seats in tandem', 'A Piper Cub is commonly painted yellow'],
             answers: ['1', '0', '1']
@@ -26,8 +27,11 @@ $(function() {
     $("#military").click(function () {
         score = 0;
         i = 0;
-        document.getElementById("next").disabled = false;
+        $(document.getElementById("next").disabled = false);
         $(document.getElementById("l1")).text("Question: ");
+        $(document.getElementById("l1")).show();
+        $(document.getElementById("l4")).show();
+        $(document.getElementById("l5")).hide();
         window.category = {
             questions: ['An F-16 has two vertical tails', 'A T-6 Texan is a nose wheel military trainer', 'The A-10 Thunderbolt II is commonly referred to as the "Warthog"'],
             answers: ['0', '0', '1']
@@ -39,8 +43,11 @@ $(function() {
     $("#common").click(function () {
         score = 0;
         i = 0;
-        document.getElementById("next").disabled = false;
+        $(document.getElementById("next").disabled = false);
         $(document.getElementById("l1")).text("Question: ");
+        $(document.getElementById("l1")).show();
+        $(document.getElementById("l4")).show();
+        $(document.getElementById("l5")).hide();
         window.category = {
             questions: ['The most common commercial aircraft is the Boeing 737', 'The most common private aircraft is the Cessna 172', 'A Cessna is an example of an "experimental" aircraft'],
             answers: ['1', '1', '0']
@@ -52,8 +59,11 @@ $(function() {
     $("#custom").click(function () {
         score = 0;
         i = 0;
-        document.getElementById("next").disabled = false;
+        $(document.getElementById("next").disabled = false);
         $(document.getElementById("l1")).text("Question: ");
+        $(document.getElementById("l1")).show();
+        $(document.getElementById("l4")).show();
+        $(document.getElementById("l5")).hide();
         window.category = {
             questions: ['The only living exact replica of the Gee Bee Z was built by Jeff Eicher', 'The P-51 Mustang was built for air racing', 'The B-2 Spirit Bomber is still in service'],
             answers: ['1', '0', '1']
@@ -63,7 +73,13 @@ $(function() {
     });
 
     $("#next").click(function () {
+        if(document.getElementById("Radio1").checked == false && document.getElementById("Radio2").checked == false){
+            alert("Please Select True or False Before Proceeding");
+            return
+        }
+
         $(document.getElementById("l2")).text("");
+        $(document.getElementById("l3")).text("");
         if(i > window.category.questions.length - 1){
             $(document.getElementById("q1A")).text(score);
             return;
@@ -81,6 +97,12 @@ $(function() {
             score = score + 1;
             $(document.getElementById("l2")).text("Correct!");
         }
+        if ((document.getElementById("Radio1").checked) && (window.category.answers[i]) == 0){
+            $(document.getElementById("l3")).text("Incorrect");
+        }
+        if ((document.getElementById("Radio2").checked) && (window.category.answers[i]) == 1){
+            $(document.getElementById("l3")).text("Incorrect");
+        }
         document.getElementById("Radio1").checked = false;
         document.getElementById("Radio2").checked = false;
         i = i + 1;
@@ -91,20 +113,29 @@ $(function() {
             score = 0;
             arr.push(attempted);
             attempted = "";
-            document.getElementById("next").disabled = true;
+            $(document.getElementById("next").disabled = true);
             return;
         }
         $(document.getElementById("q1A")).text(window.category.questions[i]);
     });
 
     $("#results").click(function () {
-        $(document.getElementById("l1")).text("Your total score is: ");
-        $(document.getElementById("q1A")).text(totalScore);
+        $(document.getElementById("l1")).show()
+        $(document.getElementById("l2")).text("");
+        $(document.getElementById("l3")).text("");
+        $(document.getElementById("l1")).text("Your total score is: ").css("font-weight","Bold");
+        $(document.getElementById("q1A")).text(totalScore).css("fontSize", "50px").css('color', 'orange');
+        $(document.getElementById("q1A")).animate({"margin-left": '450'});
+        $(document.getElementById("l4")).hide();
         var printer = "";
         for (i = 0; i < arr.length; i++) {
            printer = printer + "\n" + arr[i];
         }
         $(document.getElementById("side")).text("Questions Answered:" + printer);
+        $(document.getElementById("common").disabled = true);
+        $(document.getElementById("custom").disabled = true);
+        $(document.getElementById("military").disabled = true);
+        $(document.getElementById("home").disabled = true);
     });
 
     $("#reset").click(function () {
---
+++ hw6/readme.txt
@@ -0,0 +1,15 @@
+Jensen Eicher
+Jquery Basics
+10/2/2020
+
+Browser Selected: Google Chrome
+
+Extra Credit:
+    I decided to pursue a quiz that has multiple questions per category. This made the javascript substantially
+    more complicated. I stored each set of questions and answers in an array and used indexing based on the length of those arrays.
+    This allows the quiz to be easily added to with more questions by simply appending to the arrays. I also
+    decided to create a results page and a play again button that will reset the quiz and allow you to play again.
+    I hope that the extra complexity met the requirements! Thanks!
+
+
+
